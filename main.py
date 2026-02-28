fd3d2d7606a1b7818c874a7fabe3452005e853d6
Bafi Hoang <bafi-hng@users.noreply.github.com>
2026-02-28 20:27:39 +0100

    added boundary conditions

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 
 pygame.init()
+
 screen = pygame.display.set_mode((1280, 720))
+screen_rect = screen.get_rect()
+
 clock = pygame.time.Clock()
 running = True
 
-speed = 300
+speed = 400
 dt = 0
 
 # player object
@@ -36,6 +39,15 @@ while running:
     if keys[pygame.K_RIGHT]:
         player_rect.x += dist
 
+    if player_rect.left <= screen_rect.left:
+        player_rect.left = screen_rect.left
+    if player_rect.right >= screen_rect.right:
+        player_rect.right = screen_rect.right
+    if player_rect.top <= screen_rect.top:
+        player_rect.top = screen_rect.top
+    if player_rect.bottom >= screen_rect.bottom:
+        player_rect.bottom = screen_rect.bottom
+
     pygame.display.flip()
 
     dt = clock.tick(60) / 1000
@@ -45,5 +57,3 @@ while running:
             running = False
 
 pygame.quit()
-
-    
