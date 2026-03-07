28b3036023fa0af34bc19ec8e63e10f9d31f0ce1
Bafi Hoang <bafi-hng@users.noreply.github.com>
2026-03-07 11:02:41 +0100

    added two moveable paddles

--- main.py
+++ main.py
@@ -21,11 +21,29 @@ pong_rect = pong_surf.get_rect()
 pong_rect.x = screen.get_width() / 2
 pong_rect.y = screen.get_height() / 2
 
+# left paddle object
+x_start = 20
+left_paddle_surf = pygame.Surface((10, 60))
+left_paddle_rect = pygame.Rect(0, 0, 10, 60)
+pygame.draw.rect(left_paddle_surf, "white", left_paddle_rect)
+
+left_paddle_rect.x = x_start
+left_paddle_rect.y = screen.get_height() / 2
+
+# right paddle object
+right_paddle_surf = pygame.Surface((10, 60))
+right_paddle_rect = pygame.Rect(0, 0, 10, 60)
+pygame.draw.rect(right_paddle_surf, "white", right_paddle_rect)
+
+right_paddle_rect.x = screen.get_width() - (x_start + right_paddle_rect.width)
+right_paddle_rect.y = screen.get_height() / 2
 
 while running:
 
     screen.fill((0,0,0))
     screen.blit(pong_surf, pong_rect)
+    screen.blit(left_paddle_surf, left_paddle_rect)
+    screen.blit(right_paddle_surf, right_paddle_rect)
 
     pong_rect.x += speed_x * dt
     pong_rect.y += speed_y * dt
@@ -35,6 +53,16 @@ while running:
     if pong_rect.top <= screen_rect.top or pong_rect.bottom >= screen_rect.bottom:
         speed_y *= -1
 
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        left_paddle_rect.y -= 400 * dt
+    if keys[pygame.K_s]:
+        left_paddle_rect.y += 400 * dt
+    if keys[pygame.K_UP]:
+        right_paddle_rect.y -= 400 * dt
+    if keys[pygame.K_DOWN]:
+        right_paddle_rect.y += 400 * dt
 
     pygame.display.flip()
 
